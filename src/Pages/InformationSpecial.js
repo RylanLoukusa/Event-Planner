54bb01c032731d2f518b9e8108e0bdd5a1e5aeb6
Josie <josieadaliagreen@gmail.com>
2024-04-11 16:04:02 -0500

    more special booking edits

--- src/Pages/InformationSpecial.js
+++ src/Pages/InformationSpecial.js
@@ -1,40 +1,57 @@
 
-import { React } from "react";
+import { React, useState } from "react";
 import "../Styles/Information.css"
 
 const InformationSpecial = () => {
-    return (
-        <div className="SelectionView">
-            <h1 className="title-text">YOUR INFORMATION</h1><br />
-        <div>
-        <label className="InputPrompt">First Name</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">Last Name</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">Email</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">Address</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">City</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">State</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            <label className="InputPrompt">Zip Code</label>
-            <input id="party" className = "Input" defaultValue="enter"/>
-            
-            <label className="Header">What type of party do you want to request?</label>
-            <select>
-                <option id="inputSpecial">select</option>
-                <option id="inputSpecial">Field Trip</option>
-                <option id="inputSpecial">Third Floor Event</option>
-                <option id="inputSpecial">Book an Auditorium</option>
-            </select>
-        </div>
-            
-                <br/>
-            <button className="buttons">Submit</button>
-        </div>
-    );
+    const [first_name, setfirst_Name] = useState('');
+    const [last_name, setlast_Name] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zipCode, setZIPCode] = useState('');
+    const handleSubmit = (e) => {
+    
+        e.preventDefault();
+
+        console.log(`Form submitted, ${first_name}, ${last_name}, ${email}, ${address}, ${city}, ${state}, ${zipCode}`);    
+
+    }
+  
+        return (
+            <div className="SelectionView">
+                    <h1 className="title-text">Request a Special Event</h1><br />
+                
+            <form onSubmit = {handleSubmit}>
+            <label className="InputPrompt">First Name</label>
+                    <input className="Input" onChange = {(e) => setfirst_Name(e.target.value)} value = {first_name}></input>
+                    <label className="InputPrompt">Last Name</label>
+                    <input className="Input" onChange = {(e) => setlast_Name(e.target.value)} value = {last_name}></input>
+                    <label className="InputPrompt">Email</label>
+                    <input className="Input" onChange = {(e) => setEmail(e.target.value)} value = {email}></input>
+                    <label className="InputPrompt">Address</label>
+                    <input className="Input" onChange = {(e) => setAddress(e.target.value)} value = {address}></input>
+                    <label className="InputPrompt">City</label>
+                    <input className="Input" onChange = {(e) => setCity(e.target.value)} value = {city}></input>
+                    <label className="InputPrompt">State</label>
+                    <input className="Input" onChange = {(e) => setState(e.target.value)} value = {state}></input>
+                    <label className="InputPrompt">Zip Code</label>
+                    <input className="Input" onChange = {(e) => setZIPCode(e.target.value)} value = {zipCode}></input>
+                    
+                    <label className="Header">What type of party do you want to request?</label>
+                    <select required = "required">
+                        <option id="inputSpecial">select</option>
+                        <option id="inputSpecial">Field Trip</option>
+                        <option id="inputSpecial">Third Floor Event</option>
+                        <option id="inputSpecial">Book an Auditorium</option>
+                    </select>
+                        <br/>
+                    <button type='submit' className="buttons">submit</button>
+            </form>
+                
+                </div>
+            );
+
 }
 
 export default InformationSpecial;
