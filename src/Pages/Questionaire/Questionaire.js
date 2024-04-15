f4f5c592e74744584bdac5c55a9c47b0a3ebe02a
Green <josiegreen@twinsbaseball.com>
2024-04-15 16:01:20 -0500

    Updates to Questionnaire

--- src/Pages/Questionaire/Questionaire.js
+++ src/Pages/Questionaire/Questionaire.js
@@ -1,35 +1,41 @@
-import QuestionnaireTextField from "../../Components/QuestionnaireTextField.jsx";
+import QuestionnaireTextField from '../../Components/QuestionnaireTextField.jsx';
 import { useNavigate } from 'react-router-dom';
-import {Select} from 'react-select';
+import { Select } from 'react-select';
 
-import { React } from "react";
-import GlobalStyles from "../Global.module.css";
+import { React } from 'react';
+import GlobalStyles from '../Global.module.css';
 
 const Questionaire = () => {
-    let navigate = useNavigate();
-    return (
-        <div className={GlobalStyles.setup}>
-            <h1 className={GlobalStyles.titleText}>Questionnaire</h1><br />
-            <label>What is your birthday child's name and age? </label>
-            <input required/>
-            <label>How many children will be attending the event? </label>
-            <input required />
-            <label>How many adults will be attending the event?</label>
-            <input required/>
-            <label>What movie would you like to see?</label>
-            <input required/>
+  let navigate = useNavigate();
+  return (
+    <div className={GlobalStyles.setup}>
+      <form onSubmit={() => navigate('/Summary')}>
+        <h1 className={GlobalStyles.titleText}>Questionnaire</h1>
+        <label className={GlobalStyles.inputPrompt}>What is your birthday child's name and age? </label>
+        <input required placeholder='Enter' />
+        <label className={GlobalStyles.inputPrompt}>How many children will be attending the event? </label>
+        <input required placeholder='Enter' />
+        <label className={GlobalStyles.inputPrompt}>How many adults will be attending the event?</label>
+        <input required placeholder='Enter' />
+        <label className={GlobalStyles.inputPrompt}>What movie would you like to see?</label>
+        <input required placeholder='Enter' />
 
-            <Select />
+        <Select />
 
-            <QuestionnaireTextField label="Additional Questions (optional)" />
+        <QuestionnaireTextField label='Additional Questions (optional)' />
 
-            <button className={GlobalStyles.button} onClick={() => navigate('/Summary')}>Next: Book Spot</button>
+        <button className={GlobalStyles.button} type='submit'>
+          View booking summary
+        </button>
+      </form>
 
-            <p>Booking TIME on DATE</p>
+      <p>Booking TIME on DATE</p>
 
-            <button className={GlobalStyles.specialButton}>Change or Cancel</button>
-        </div>
-    );
-}
+      <button className={GlobalStyles.specialButton} onClick={() => navigate('/')}>
+        Change or Cancel
+      </button>
+    </div>
+  );
+};
 
 export default Questionaire;
